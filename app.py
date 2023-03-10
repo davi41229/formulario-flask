debe36a0a511a2abfae26d7dbda55d16f61fc2a2
Davi Nascimento <68043001+davi41229@users.noreply.github.com>
2023-03-10 01:42:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
 from config import email,senha
-from app import app, os
+from app import os
 
 app = Flask(__name__)
 app.secret_key = 'sucodeuva'
